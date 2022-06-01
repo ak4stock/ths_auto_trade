25a2a7ec748481026db09cc9310979ab12cbfc53
ak4stock <89284534+ak4stock@users.noreply.github.com>
2022-06-01 20:14:02 +0800

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from loguru import logger
 import easytrader
 from easytrader import refresh_strategies
 from easytrader import grid_strategies
-    
+     
  
 logger.add('./logs/api_{time}.log', rotation='00:00', encoding='utf-8')
 
