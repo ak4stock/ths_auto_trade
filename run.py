00a511a4c13aedbc07c96afc7e8cd763846b45cc
ak4stock <89284534+ak4stock@users.noreply.github.com>
2022-07-10 20:43:41 +0800

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ import easytrader
 from easytrader import refresh_strategies 
 from easytrader import grid_strategies 
      
-  
+   
 logger.add('./logs/api_{time}.log', rotation='00:00', encoding='utf-8') 
 
 app = Flask(__name__)
